52fc622a92e44b0eef8f641200dd3a49073a0835
azimabdurasulov <azimjon20050604@gmail.com>
2022-09-26 21:32:27 +0500

    2-topshiriq'

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,5 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+answer = 3 * (7 / 5 - 9 / 4)
+answer = round(answer, 2) 
+print(answer)
