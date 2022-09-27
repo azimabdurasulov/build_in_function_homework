5b79d0d6671acd70a98b23e8e645289a11a63d09
azimabdurasulov <azimjon20050604@gmail.com>
2022-09-27 10:56:04 +0500

    6-topshiriq

--- build_func06.py
+++ build_func06.py
@@ -1,5 +1,5 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func06
-s = 3.456
-answer = round(s, 2)
+a = 3.456
+answer = round(a, 2)
 print(answer)
