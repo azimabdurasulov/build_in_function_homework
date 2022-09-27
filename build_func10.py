b3168ff87d2cfff371dd1fd586af85af6b49e138
azimabdurasulov <azimjon20050604@gmail.com>
2022-09-27 11:05:34 +0500

    10-topshiriq

--- build_func10.py
+++ build_func10.py
@@ -2,6 +2,6 @@
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
 x = 8
 y = 4
-answer = round((3 * pow(y,  1 / 2) + pow(x,  2 / 3)), 2)
+answer = (3 * pow(y,  1 / 2) + pow(x,  2 / 3))
 
 print(answer)
